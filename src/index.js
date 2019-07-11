a8fe74ff5b385be2434e8e9e95818be285e70237
Taimoor Farras <farrastaimoor@gmail.com>
2019-07-11 11:32:51 +0300

    remove comments from index.js

--- src/index.js
+++ src/index.js
@@ -12,19 +12,6 @@ import {
   DELETE_MANY,
 } from 'react-admin';
 
-/**
- * Maps react-admin queries to a simple REST API
- *
- * The REST dialect is similar to the one of FakeRest
- * @see https://github.com/marmelab/FakeRest
- * @example
- * GET_LIST     => GET http://my.api.url/posts?sort=['title','ASC']&range=[0, 24]
- * GET_ONE      => GET http://my.api.url/posts/123
- * GET_MANY     => GET http://my.api.url/posts?filter={ids:[123,456,789]}
- * UPDATE       => PUT http://my.api.url/posts/123
- * CREATE       => POST http://my.api.url/posts
- * DELETE       => DELETE http://my.api.url/posts/123
- */
 export default (apiUrl, httpClient = fetchUtils.fetchJson) => {
 
   const composeFilter = (paramsFilter) => {
@@ -33,12 +20,7 @@ export default (apiUrl, httpClient = fetchUtils.fetchJson) => {
       const splitKey = key.split('||');
       let ops = splitKey[1] ? splitKey[1] : 'cont';
       let field = splitKey[0];
-      // code below allows multifield filters in react admin (date range for example). Here is an example:
-      //
-      // <InputRow source="_created_range" label="Cretaion date">
-      //   <DateInput source="_created_range.created||gte" label="From" options={{ format: 'dd/MM/YYYY' }} />
-      //   <DateInput source="_created_range.created||lte" label="To" options={{ format: 'dd/MM/YYYY' }} />
-      // </InputRow>
+
       if (field.indexOf('_') === 0 && field.indexOf('.') > -1) {
         field = field.split(/\.(.+)/)[1];
       }
@@ -47,12 +29,6 @@ export default (apiUrl, httpClient = fetchUtils.fetchJson) => {
     return filter;
   };
 
-  /**
-   * @param {String} type One of the constants appearing at the top if this file, e.g. 'UPDATE'
-   * @param {String} resource Name of the resource to fetch, e.g. 'posts'
-   * @param {Object} params The data request params, depending on the type
-   * @returns {Object} { url, options } The HTTP request parameters
-   */
   const convertDataRequestToHTTP = (type, resource, params) => {
     let url = '';
     const options = {};
@@ -140,13 +116,6 @@ export default (apiUrl, httpClient = fetchUtils.fetchJson) => {
     return { url, options };
   };
 
-  /**
-   * @param {Object} response HTTP response from fetch()
-   * @param {String} type One of the constants appearing at the top if this file, e.g. 'UPDATE'
-   * @param {String} resource Name of the resource to fetch, e.g. 'posts'
-   * @param {Object} params The data request params, depending on the type
-   * @returns {Object} Data response
-   */
   const convertHTTPResponse = (response, type, resource, params) => {
     const { headers, json } = response;
     switch (type) {
@@ -163,14 +132,7 @@ export default (apiUrl, httpClient = fetchUtils.fetchJson) => {
     }
   };
 
-  /**
-   * @param {string} type Request type, e.g GET_LIST
-   * @param {string} resource Resource name, e.g. "posts"
-   * @param {Object} payload Request parameters. Depends on the request type
-   * @returns {Promise} the Promise for a data response
-   */
   return (type, resource, params) => {
-    // simple-rest doesn't handle filters on UPDATE route, so we fallback to calling UPDATE n times instead
     if (type === UPDATE_MANY) {
       return Promise.all(
           params.ids.map(id =>
@@ -183,7 +145,6 @@ export default (apiUrl, httpClient = fetchUtils.fetchJson) => {
         data: responses.map(response => response.json),
       }));
     }
-    // simple-rest doesn't handle filters on DELETE route, so we fallback to calling DELETE n times instead
     if (type === DELETE_MANY) {
       return Promise.all(
           params.ids.map(id =>
